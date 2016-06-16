fd2d3e8c958f0c301a3301a47fceac139cc7283b
Bruno Araújo <brunoluan@gmail.com>
2016-06-15 21:01:17 -0300

    ACesso alteracao de perfil topo

--- resources/views/layouts/includes/header.blade.php
+++ resources/views/layouts/includes/header.blade.php
@@ -1,66 +1,28 @@
 <header class="main-header">
-   <!-- Logo -->
-   <a href="{{url('/')}}" class="logo">
-      <!-- mini logo for sidebar mini 50x50 pixels -->
-      <span class="logo-mini"><b>A</b>DM</span>
-      <!-- logo for regular state and mobile devices -->
-      <span class="logo-lg"><b>Admin</b></span>
-   </a>
-   <!-- Header Navbar: style can be found in header.less -->
    <nav class="navbar navbar-static-top">
-      <!-- Sidebar toggle button-->
-      <a href="#" class="sidebar-toggle" data-toggle="offcanvas" role="button">
-         <span class="sr-only">Toggle navigation</span>
-      </a>
-
-      <div class="container-fluid">
-         <!-- Collect the nav links, forms, and other content for toggling -->
-         <div class="collapse navbar-collapse" id="navbar-collapse">
-
-            <ul class="nav navbar-nav navbar-right">
-              <li class="dropdown user user-menu">
-                  <a href="#" class="dropdown-toggle" data-toggle="dropdown" aria-expanded="true">
-                    <img src="../../dist/img/user2-160x160.jpg" class="user-image" alt="User Image">
-                    <span class="hidden-xs">Alexander Pierce</span>
-                  </a>
-                  <ul class="dropdown-menu">
-                    <!-- User image -->
-                    <li class="user-header">
-                      <img src="../../dist/img/user2-160x160.jpg" class="img-circle" alt="User Image">
-
-                      <p>
-                        Alexander Pierce - Web Developer
-                        <small>Member since Nov. 2012</small>
-                      </p>
-                    </li>
-                    <!-- Menu Body -->
-                    <li class="user-body">
-                      <div class="row">
-                        <div class="col-xs-4 text-center">
-                          <a href="#">Followers</a>
-                        </div>
-                        <div class="col-xs-4 text-center">
-                          <a href="#">Sales</a>
-                        </div>
-                        <div class="col-xs-4 text-center">
-                          <a href="#">Friends</a>
-                        </div>
-                      </div>
-                      <!-- /.row -->
-                    </li>
-                    <!-- Menu Footer-->
-                    <li class="user-footer">
-                      <div class="pull-left">
-                        <a href="#" class="btn btn-default btn-flat">Profile</a>
-                      </div>
-                      <div class="pull-right">
-                        <a href="#" class="btn btn-default btn-flat">Sign out</a>
-                      </div>
-                    </li>
-                  </ul>
-                </li>
+      <div class="navbar-custom-menu">
+        <ul class="nav navbar-nav">
+          <li class="dropdown notifications-menu">
+            <a href="#" class="dropdown-toggle" data-toggle="dropdown" aria-expanded="true">
+              <i class="fa fa-user"></i> {{ Auth::user()->pessoa->pes_nome }}
+            </a>
+            <ul class="dropdown-menu" style="width:100px;height:autopx">
+              <li>
+                <ul class="menu">
+                  <li>
+                    <a href="{{url('/')}}/seguranca/profile">
+                      <i class="fa fa-edit text-aqua"></i> Perfil
+                    </a>
+                  </li>
+                  <li>
+                    <a href="{{url('/')}}/logout">
+                      <i class="fa fa-close text-red"></i> Sair
+                    </a>
+                  </li>
+              </li>
             </ul>
-         </div><!-- /.navbar-collapse -->
-      </div><!-- /.container-fluid -->
+          </li>
+        </ul>
+      </div>
    </nav>
 </header>
