222fda49ca6c70c831cc29a36ca23bbb31512c61
Willian Mano <willianmanoaraujo@gmail.com>
2016-06-16 15:13:08 -0300

    Finalizado index de pessoas

--- modulos/Geral/Repositories/PessoaRepository.php
+++ modulos/Geral/Repositories/PessoaRepository.php
@@ -2,8 +2,10 @@
 
 namespace Modulos\Geral\Repositories;
 
+use Illuminate\Support\Facades\DB;
 use Modulos\Core\Repository\BaseRepository;
 use Modulos\Geral\Models\Pessoa;
+use Stevebauman\EloquentTable\TableCollection;
 
 class PessoaRepository extends BaseRepository
 {
@@ -11,4 +13,71 @@ class PessoaRepository extends BaseRepository
     {
         $this->model = $pessoa;
     }
+
+    public function paginate($sort = null, $search = null)
+    {
+        $result = $this->model->leftJoin('gra_documento', function ($join) {
+               $join->on('pes_id', '=', 'doc_pes_id')->on('doc_tpd_id', '=', 1, 'and', true);
+            });
+
+        if (!empty($search)) {
+            foreach ($search as $value) {
+                if ($value['field'] == 'pes_cpf') {
+                    $result = $result->where('doc_conteudo', '=', $value['term']);
+                    continue;
+                }
+
+                switch ($value['type']) {
+                    case 'like':
+                        $result = $result->where($value['field'], $value['type'], "%{$value['term']}%");
+                        break;
+                    default:
+                        $result = $result->where($value['field'], $value['type'], $value['term']);
+                }
+            }
+        }
+
+        if (!empty($sort)) {
+            $result = $result->orderBy($sort['field'], $sort['sort']);
+        }
+
+        $result = $result->paginate(15);
+
+        return $result;
+    }
+
+//    public function paginate($sort = null, $search = null)
+//    {
+//        $result = DB::table('gra_pessoa')
+//            ->leftJoin('gra_documento', function ($join) {
+//               $join->on('pes_id', '=', 'doc_pes_id')->on('doc_tpd_id', '=', 2, 'and', true);
+//            });
+//
+//        if (!empty($search)) {
+//            foreach ($search as $value) {
+//                if ($value['field'] == 'pes_cpf') {
+//                    $result = $result->where('doc_conteudo', '=', $value['term']);
+//                    continue;
+//                }
+//
+//                switch ($value['type']) {
+//                    case 'like':
+//                        $result = $result->where($value['field'], $value['type'], "%{$value['term']}%");
+//                        break;
+//                    default:
+//                        $result = $result->where($value['field'], $value['type'], $value['term']);
+//                }
+//            }
+//        }
+//
+//        if (!empty($sort)) {
+//            $result = $result->orderBy($sort['field'], $sort['sort']);
+//        }
+//
+//        $result = $result->paginate(15);
+//
+//        $result->setCollection(new TableCollection($result->items()));
+//
+//        return $result;
+//    }
 }
--- modulos/Geral/Views/pessoas/index.blade.php
+++ modulos/Geral/Views/pessoas/index.blade.php
@@ -5,7 +5,7 @@
 @stop
 
 @section('subtitle')
-    Cadastro de Pessoa
+    Gerenciamento de Pessoas
 @stop
 
 @section('actionButton')
@@ -13,5 +13,84 @@
 @stop
 
 @section('content')
-    bruno
+    <div class="box box-primary">
+        <div class="box-header with-border">
+            <h3 class="box-title"><i class="fa fa-filter"></i> Filtrar dados</h3>
+
+            <div class="box-tools pull-right">
+                <button type="button" class="btn btn-box-tool" data-widget="collapse"><i class="fa fa-minus"></i>
+                </button>
+            </div>
+            <!-- /.box-tools -->
+        </div>
+        <!-- /.box-header -->
+        <div class="box-body">
+            <div class="row">
+                <form method="GET" action="{{ url('/geral/pessoas/index') }}">
+                    <div class="col-md-3">
+                        <input type="text" class="form-control" name="pes_cpf" id="pes_cpf" value="{{Input::get('pes_cpf')}}" placeholder="CPF">
+                    </div>
+                    <div class="col-md-3">
+                        <input type="text" class="form-control" name="pes_nome" id="pes_nome" value="{{Input::get('pes_nome')}}" placeholder="Nome">
+                    </div>
+                    <div class="col-md-3">
+                        <input type="email" class="form-control" name="pes_email" id="pes_email" value="{{Input::get('pes_email')}}" placeholder="Email">
+                    </div>
+                    <div class="col-md-3">
+                        <input type="submit" class="form-control btn-primary" value="Buscar">
+                    </div>
+                </form>
+            </div>
+        </div>
+        <!-- /.box-body -->
+    </div>
+    @if($tableData->count())
+        <div class="box box-primary">
+            <div class="box-header">
+                {!!
+                    $tableData->columns(array(
+                        'pes_id' => '#',
+                        'pes_nome' => 'Nome',
+                        'doc_conteudo' => 'CPF',
+                        'pes_email' => 'Email',
+                        'pes_action' => 'Ações'
+                    ))
+                    ->modifyCell('pes_action', function() {
+                        return array('style' => 'width: 140px;');
+                    })
+                    ->means('pes_action', 'pes_id')
+                    ->modify('pes_action', function($id) {
+                        return ActionButton::grid([
+                                'type' => 'SELECT',
+                                'config' => [
+                                    'classButton' => 'btn-default',
+                                    'label' => 'Selecione'
+                                ],
+                                'buttons' => [
+                                    [
+                                        'classButton' => '',
+                                        'icon' => 'fa fa-pencil',
+                                        'action' => '/geral/pessoas/edit/' . $id,
+                                        'label' => 'Editar',
+                                        'method' => 'get'
+                                    ],
+                                    [
+                                        'classButton' => 'btn-delete text-red',
+                                        'icon' => 'fa fa-trash',
+                                        'action' => '/geral/pessoas/delete',
+                                        'id' => $id,
+                                        'label' => 'Excluir',
+                                        'method' => 'post'
+                                    ]
+                                ]
+                            ]);
+                    })
+                    ->sortable(array('pes_id', 'pes_nome'))
+                    ->render()
+                !!}
+            </div>
+        </div>
+
+        <div class="text-center">{!! $tableData->appends(Input::except('page'))->links() !!}</div>
+    @endif
 @stop
