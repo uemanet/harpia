dc117666a500ca63c46a66668778c7f67e6af7ab
Willian Mano <willianmanoaraujo@gmail.com>
2016-06-16 18:24:43 -0300

    Criado controller index no geral

---
+++ modulos/Geral/Views/index/index.blade.php
@@ -0,0 +1,13 @@
+@extends('layouts.modulos.seguranca')
+
+@section('title')
+    Módulo Geral
+@stop
+
+@section('subtitle')
+    Módulo de configurações gerais
+@stop
+
+@section('content')
+
+@stop
--- modulos/Geral/routes.php
+++ modulos/Geral/routes.php
@@ -3,5 +3,6 @@
 Route::group(['prefix' => 'geral', 'middleware' => ['auth']], function () {
     Route::controllers([
         'pessoas' => '\Modulos\Geral\Http\Controllers\PessoasController',
+        'index' => '\Modulos\Geral\Http\Controllers\IndexController',
     ]);
 });
