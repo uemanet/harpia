9b7f7a930846260122f4df8c9bcb914d8e8c76e3
Pedro Fellipe <pedro_fellipe123@hotmail.com>
2016-08-16 16:44:15 -0300

    model Polo

---
+++ modulos/Academico/Models/Polo.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Modulos\Academico\Models;
+
+use Modulos\Core\Model\BaseModel;
+
+class Polo extends BaseModel
+{
+    protected $table = 'acd_polos';
+
+    protected $primaryKey = 'pol_id';
+
+    protected $fillable = ['pol_nome'];
+
+    protected $searchable = [
+        'pol_nome' => 'like'
+    ];
+}
