e82389e65677c82ff364f3da46383b0b5fbf0a0e
Lucas S. Vieira <lucassouzavieiraengcomp@gmail.com>
2016-08-16 16:50:40 -0300

    Model departamento

---
+++ modulos/Academico/Models/Departamento.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Modulos\Academico\Models;
+
+use Modulos\Core\Model\BaseModel;
+
+class Modulo extends BaseModel
+{
+    protected $table = 'acd_departamentos';
+
+    protected $primaryKey = 'dep_id';
+
+    protected $fillable = ['dep_cen_id', 'dep_prf_diretor','dep_nome'];
+
+    protected $searchable = [
+        'dep_nome' => 'like'
+    ];
+
+    public function centro()
+    {
+        $this->belongsTo('Modulos\Academico\Models\Centro', 'dep_cen_id', 'cen_id');
+    }
+
+    public function diretor()
+    {
+        $this->belongsTo('Modulos\Academico\Models\Professor', 'dep_prf_diretor', 'prf_id');
+    }
+
+}
