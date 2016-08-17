9fb8c2622d9775615d0f0a5d299d9b63a03c9861
Lucas S. Vieira <lucassouzavieiraengcomp@gmail.com>
2016-08-17 15:49:35 -0300

    Adicionados Seeds de Academico

---
+++ modulos/Academico/Database/Seeds/ModalidadeTableSeeder.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Modulos\Academico\Database\Seeds;
+
+use Illuminate\Database\Seeder;
+use Modulos\Academico\Models\Modalidade;
+
+class ModalidadeTableSeeder extends Seeder
+{
+    public function run()
+    {
+        
+    }
+}
---
+++ modulos/Academico/Database/Seeds/NivelCursoTableSeeder.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Modulos\Academico\Database\Seeds;
+
+use Illuminate\Database\Seeder;
+use Modulos\Academico\Models\NivelCurso;
+
+class NivelCursoTableSeeder extends Seeder
+{
+    public function run()
+    {
+
+    }
+}
