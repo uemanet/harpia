eef595e05007cab637162b5f8b57cc5f2bcfaa2f
Lucas S. Vieira <lucassouzavieiraengcomp@gmail.com>
2016-08-17 16:01:18 -0300

    Seeds de NiveisCurso e Modalidade

--- modulos/Academico/Database/Seeds/ModalidadeTableSeeder.php
+++ modulos/Academico/Database/Seeds/ModalidadeTableSeeder.php
@@ -9,6 +9,16 @@ class ModalidadeTableSeeder extends Seeder
 {
     public function run()
     {
-        
+        $modalidade = new Modalidade();
+        $modalidade->mdl_nome = 'Presencial';
+        $modalidade->save();
+
+        $modalidade = new Modalidade();
+        $modalidade->mdl_nome = 'Semi-presencial';
+        $modalidade->save();
+
+        $modalidade = new Modalidade();
+        $modalidade->mdl_nome = 'EaD';
+        $modalidade->save();
     }
 }
--- modulos/Academico/Database/Seeds/NivelCursoTableSeeder.php
+++ modulos/Academico/Database/Seeds/NivelCursoTableSeeder.php
@@ -9,6 +9,16 @@ class NivelCursoTableSeeder extends Seeder
 {
     public function run()
     {
+        $nivel = new NivelCurso();
+        $nivel->nvc_nome = 'Técnico';
+        $nivel->save();
 
+        $nivel = new NivelCurso();
+        $nivel->nvc_nome = 'Graduação';
+        $nivel->save();
+
+        $nivel = new NivelCurso();
+        $nivel->nvc_nome = 'Pós-graduação';
+        $nivel->save();
     }
 }
