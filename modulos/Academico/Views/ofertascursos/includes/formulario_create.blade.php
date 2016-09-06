1daad88dcb261a1eeb96e9a21895688d597fe693
Pedro Fellipe <pedro_fellipe123@hotmail.com>
2016-09-06 16:50:46 -0300

    Layout do formulário de ofertas atualizado

--- modulos/Academico/Views/ofertascursos/includes/formulario_create.blade.php
+++ modulos/Academico/Views/ofertascursos/includes/formulario_create.blade.php
@@ -22,24 +22,22 @@
     </div>
 </div>
 <div class="row">
-        <div class="form-group col-md-12 @if ($errors->has('polos[]')) has-error @endif">
+        <div class="form-group col-md-9 @if ($errors->has('polos[]')) has-error @endif">
             {!! Form::label('polos[]', 'Polos*', ['class' => 'control-label']) !!}
             <div class="controls">
                 {!! Form::select('polos[]', $polos, old('polos[]'), ['class' => 'form-control', 'multiple' => 'multiple']) !!}
                 @if ($errors->has('polos[]')) <p class="help-block">{{ $errors->first('polos[]') }}</p> @endif
             </div>
         </div>
-</div>
-</dic>
-<div class="row">
-    <div class="form-group col-md-12 @if ($errors->has('ofc_ano')) has-error @endif">
-        {!! Form::label('ofc_ano', 'Ano*', ['class' => 'control-label']) !!}
-        <div class="controls">
-            {!! Form::number('ofc_ano', old('ofc_ano'), ['class' => 'form-control']) !!}
-            @if ($errors->has('ofc_ano')) <p class="help-block">{{ $errors->first('ofc_ano') }}</p> @endif
+        <div class="form-group col-md-3 @if ($errors->has('ofc_ano')) has-error @endif">
+            {!! Form::label('ofc_ano', 'Ano*', ['class' => 'control-label']) !!}
+            <div class="controls">
+                {!! Form::number('ofc_ano', old('ofc_ano'), ['class' => 'form-control']) !!}
+                @if ($errors->has('ofc_ano')) <p class="help-block">{{ $errors->first('ofc_ano') }}</p> @endif
+            </div>
         </div>
-    </div>
 </div>
+
 <div class="row">
     <div class="form-group col-md-12">
         {!! Form::submit('Salvar dados', ['class' => 'btn btn-primary pull-right']) !!}
