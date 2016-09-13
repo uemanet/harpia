a9e9d805e330d831966dd5baee58153c8431921d
Pedro Fellipe <pedro_fellipe123@hotmail.com>
2016-09-13 17:00:44 -0300

    Criada obrigatoriedade de adicionar polos

--- modulos/Academico/Http/Requests/OfertaCursoRequest.php
+++ modulos/Academico/Http/Requests/OfertaCursoRequest.php
@@ -27,7 +27,8 @@ class OfertaCursoRequest extends BaseRequest
             'ofc_crs_id' => 'required',
             'ofc_mtc_id' => 'required',
             'ofc_mdl_id' => 'required',
-            'ofc_ano' => 'integer|min:1|max:9999'
+            'ofc_ano' => 'integer|min:1|max:9999',
+            'polos[]' => 'required'
         ];
 
         return $rules;
