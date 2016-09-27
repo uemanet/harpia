04c3979eeac8610b1d1611d0e19efd83c7a607a5
Felipe Pimenta <fhpimenta12@gmail.com>
2016-09-27 14:05:42 -0300

    Correção do titulo das paginas

--- resources/views/layouts/modulos/academico.blade.php
+++ resources/views/layouts/modulos/academico.blade.php
@@ -5,7 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
 
-    <title>Módulo ADMIN @yield('title')</title>
+    <title>Módulo Acadêmico - @yield('title')</title>
 
     <link rel="stylesheet" href="{{ asset('/css/plugins/sweetalert.css') }}" />
     <link rel="stylesheet" href="{{ asset('/css/plugins/toastr.min.css') }}" />
