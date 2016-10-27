61415fec62da36f5c071c1bb2520fa73e4c15c1c
Lucas S. Vieira <lucassouzavieiraengcomp@gmail.com>
2016-10-27 17:12:21 -0300

    Migration de Anexo

--- modulos/Geral/Repositories/AnexoRepository.php
+++ modulos/Geral/Repositories/AnexoRepository.php
@@ -10,12 +10,27 @@ use Symfony\Component\HttpFoundation\File\Exception\FileException;
 
 class AnexoRepository extends BaseRepository
 {
+
+    protected $basePath;
+
     public function __construct(Anexo $anexo)
     {
         $this->model = $anexo;
+        $this->basePath = storage_path(). DIRECTORY_SEPARATOR .'uploads' . DIRECTORY_SEPARATOR;
     }
 
     /**
+     * Array com o nome dos diretorios baseados no hash passado
+     * @param $hash
+     * @return array
+     */
+    private function hashDirectories($hash)
+    {
+        return array(substr($hash, 0, 2), substr($hash, 2, 2));
+    }
+
+    /**
+     * TODO Refatorar o metodo e corrigir os problemas encontrados ( GitLab issue #19)
      * Trata uploads guardando o arquivo no servidor e registrando na
      * base de dados
      * @param UploadedFile $uploadedFile
@@ -30,11 +45,11 @@ class AnexoRepository extends BaseRepository
         $pDir = substr($hash, 0, 2); // first Directory
         $sDir = substr($hash, 2, 2); // second Directory
 
-        $caminhoArquivo = storage_path(). DIRECTORY_SEPARATOR .'uploads' . DIRECTORY_SEPARATOR. $pDir .DIRECTORY_SEPARATOR. $sDir;
+        $caminhoArquivo = $this->basePath . $pDir .DIRECTORY_SEPARATOR. $sDir;
 
-        if (file_exists($caminhoArquivo.$hash)) {
+        if (file_exists($caminhoArquivo.DIRECTORY_SEPARATOR.$hash)) {
             if (config('app.debug')) {
-                throw new FileExistsException($caminhoArquivo.$hash);
+                throw new FileExistsException($caminhoArquivo. DIRECTORY_SEPARATOR .$hash);
             }
             flash()->error('Arquivo já existe !');
             return redirect()->back();
@@ -45,26 +60,41 @@ class AnexoRepository extends BaseRepository
                 'anx_tax_id' => $tipoAnexo,
                 'anx_nome' => $uploadedFile->getClientOriginalName(),
                 'anx_mime' => $uploadedFile->getClientMimeType(),
-                'anx_localizacao' => $caminhoArquivo
+                'anx_extensao' => $uploadedFile->getClientOriginalExtension(),
+                'anx_localizacao' => $hash
             ];
-
             $uploadedFile->move($caminhoArquivo, $hash);
-
             return $this->create($anexo);
+
         } catch (FileException $e) {
+
             if (config('app.debug')) {
                 throw $e;
             }
             flash()->error('Ocorreu um problema ao salvar o arquivo!');
+
         } catch (\Exception $e) {
+
             if (config('app.debug')) {
                 throw $e;
             }
+
             flash()->error('Ocorreu um problema ao salvar o arquivo!');
         }
     }
 
     /**
+     * TODO Implementar funcionalidade de recuperacao de arquivo
+     * @param $anexoId
+     * @return null
+     */
+    public function recuperarAnexo($anexoId)
+    {
+        return null;
+    }
+
+    /**
+     * TODO Refatorar o metodo e corrigir os problemas encontrados ( GitLab issue #19)
      * Atualiza o registro de um anexo
      * @param $anexoId
      * @param UploadedFile $uploadedFile
@@ -86,11 +116,11 @@ class AnexoRepository extends BaseRepository
         $pDir = substr($hash, 0, 2); // first Directory
         $sDir = substr($hash, 2, 2); // second Directory
 
-        $caminhoArquivo = storage_path(). DIRECTORY_SEPARATOR .'uploads' . DIRECTORY_SEPARATOR. $pDir .DIRECTORY_SEPARATOR. $sDir;
+        $caminhoArquivo = $this->basePath. $pDir .DIRECTORY_SEPARATOR. $sDir;
 
-        if (file_exists($caminhoArquivo.$hash)) {
+        if (file_exists($caminhoArquivo. DIRECTORY_SEPARATOR . $hash)) {
             if (config('app.debug')) {
-                throw new FileExistsException($caminhoArquivo.$hash);
+                throw new FileExistsException($caminhoArquivo. DIRECTORY_SEPARATOR . $hash);
             }
             flash()->error('Arquivo já existe !');
             return redirect()->back();
@@ -98,8 +128,8 @@ class AnexoRepository extends BaseRepository
 
         try {
 
-            // Exclui antigo arquivo e pasta corresṕondente
-            array_map('unlink', glob($anexo->anx_localizacao . DIRECTORY_SEPARATOR . '*'));
+            // Exclui antigo arquivo e pasta correspondente
+            array_map('unlink', glob($this->basePath . DIRECTORY_SEPARATOR . $anexo->anx_localizacao . DIRECTORY_SEPARATOR . '*'));
             array_map('rmdir', glob($anexo->anx_localizacao . DIRECTORY_SEPARATOR));
 
             // Atualiza registro com o novo arquivo
@@ -107,7 +137,8 @@ class AnexoRepository extends BaseRepository
                 'anx_tax_id' => $tipoAnexo,
                 'anx_nome' => $uploadedFile->getClientOriginalName(),
                 'anx_mime' => $uploadedFile->getClientMimeType(),
-                'anx_localizacao' => $caminhoArquivo
+                'anx_extensao' => $uploadedFile->getClientOriginalExtension(),
+                'anx_localizacao' => $hash
             ];
 
             $uploadedFile->move($caminhoArquivo, $hash);
@@ -116,16 +147,20 @@ class AnexoRepository extends BaseRepository
             if (config('app.debug')) {
                 throw $e;
             }
+
             flash()->error('Ocorreu um problema ao atualizar o arquivo!');
         } catch (\Exception $e) {
+
             if (config('app.debug')) {
                 throw $e;
             }
+
             flash()->error('Ocorreu um problema ao atualizar o arquivo!');
         }
     }
 
     /**
+     * TODO Refatorar o metodo e corrigir os problemas encontrados ( GitLab issue #19)
      * Deleta um anexo do servidor e seu registro no banco
      * @param $anexoId
      * @return int|string
@@ -149,11 +184,13 @@ class AnexoRepository extends BaseRepository
             if (config('app.debug')) {
                 throw $e;
             }
+
             flash()->error('Ocorreu um problema ao excluir o arquivo!');
         } catch (\Exception $e) {
             if (config('app.debug')) {
                 throw $e;
             }
+
             flash()->error('Ocorreu um problema ao excluir o arquivo!');
         }
     }
