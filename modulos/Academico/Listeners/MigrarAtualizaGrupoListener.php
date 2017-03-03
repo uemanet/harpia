a66c66e8db773479a4e7d9751df6d945c57c2b63
Guilherme Oliveira <guilhermeoliveira.f11@gmail.com>
2017-03-03 18:11:58 -0300

    Inserção da action update no evento de atualizar

--- modulos/Academico/Listeners/MigrarAtualizaGrupoListener.php
+++ modulos/Academico/Listeners/MigrarAtualizaGrupoListener.php
@@ -64,7 +64,7 @@ class MigrarAtualizaGrupoListener
                         }
                     }
 
-                    event(new AtualizarSyncEvent($grupo, $status, $retorno['message']));
+                    event(new AtualizarSyncEvent($grupo, $status, $retorno['message'], 'UPDATE'));
                 }
             }
         }
