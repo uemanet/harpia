60f8ff57eae0d235b8069d01880edd69ab05b4ed
Felipe Pimenta <fhpimenta12@gmail.com>
2017-04-10 15:19:19 -0300

    Select2 adicionado a todos os formulario do modulo

--- modulos/Seguranca/Views/modulos/includes/formulario.blade.php
+++ modulos/Seguranca/Views/modulos/includes/formulario.blade.php
@@ -1,3 +1,6 @@
+@section('stylesheets')
+    <link rel="stylesheet" href="{{asset('/css/plugins/select2.css')}}">
+@stop
 <div class="row">
     <div class="form-group col-md-3 @if ($errors->has('mod_nome')) has-error @endif">
         {!! Form::label('mod_nome', 'Nome do módulo*', ['class' => 'control-label']) !!}
@@ -73,3 +76,13 @@
         {!! Form::submit('Salvar dados', ['class' => 'btn btn-primary pull-right']) !!}
     </div>
 </div>
+
+@section('scripts')
+    <script src="{{asset('/js/plugins/select2.js')}}" type="text/javascript"></script>
+
+    <script type="text/javascript">
+        $(document).ready(function () {
+            $("select").select2();
+        });
+    </script>
+@stop
--- modulos/Seguranca/Views/recursos/includes/formulario_create.blade.php
+++ modulos/Seguranca/Views/recursos/includes/formulario_create.blade.php
@@ -1,3 +1,6 @@
+@section('stylesheets')
+    <link rel="stylesheet" href="{{asset('/css/plugins/select2.css')}}">
+@stop
 <div class="row">
     <div class="form-group col-md-6 @if ($errors->has('mod_id')) has-error @endif">
         {!! Form::label('mod_id', 'Módulo*', ['class' => 'control-label']) !!}
@@ -17,9 +20,12 @@
 
 @section('scripts')
     @parent
-
+    <script src="{{asset('/js/plugins/select2.js')}}" type="text/javascript"></script>
     <script type="application/javascript">
         $(document).ready(function(){
+
+            $('select').select2();
+
             $('#mod_id').prop('selectedIndex',0);
         });
     </script>
