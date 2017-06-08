6a87ca842f4b223b13456875b0336606825d0a81
PedroFellipe <pedrofellipemelo123@gmail.com>
2017-06-08 14:40:52 -0300

    migrations de diploma e certificação criadas

--- modulos/Academico/Database/Migrations/2017_03_14_152851_create_acd_registros_table.php
+++ modulos/Academico/Database/Migrations/2017_03_14_152851_create_acd_registros_table.php
@@ -17,22 +17,14 @@ class CreateAcdRegistrosTable extends Migration
             $table->increments('reg_id');
 
             $table->integer('reg_liv_id')->unsigned();
-            $table->integer('reg_mat_id')->unsigned();
+            $table->integer('reg_usr_id')->unsigned();
 
             $table->integer('reg_folha');
             $table->integer('reg_registro');
-            $table->string('reg_registro_externo')->nullable();
-            $table->string('reg_processo')->nullable();
-            $table->date('reg_data_expedicao');
-            $table->text('reg_observacao')->nullable();
-            $table->string('reg_usuario');
-            $table->date('reg_data');
-            $table->integer('reg_id_interno')->nullable();
-            $table->integer('reg_mdo_id')->unsigned()->nullable();
+            $table->string('reg_codigo_autenticidade', 255);
 
             $table->foreign('reg_liv_id')->references('liv_id')->on('acd_livros');
-            $table->foreign('reg_mat_id')->references('mat_id')->on('acd_matriculas');
-            $table->foreign('reg_mdo_id')->references('mdo_id')->on('acd_modulos_matrizes');
+            $table->foreign('reg_usr_id')->references('usr_id')->on('seg_usuarios');
 
             $table->timestamps();
         });
---
+++ modulos/Academico/Database/Migrations/2017_03_14_152852_create_acd_diplomas_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateAcdDiplomasTable extends Migration
+{
+
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('acd_diplomas', function (Blueprint $table) {
+            $table->increments('dip_id');
+
+            $table->integer('dip_reg_id')->unsigned();
+            $table->integer('dip_mat_id')->unsigned();
+
+            $table->string('dip_processo');
+            $table->string('dip_codigo_autenticidade_externo');
+
+            $table->foreign('dip_liv_id')->references('liv_id')->on('acd_livros');
+            $table->foreign('dip_usr_id')->references('usr_id')->on('seg_usuarios');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('acd_diplomas');
+    }
+}
