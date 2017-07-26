bd7092eba37485c873a86592b47c6a4bc3999098
Felipe Pimenta <fhpimenta12@gmail.com>
2017-07-26 15:57:16 -0300

    Criação da permissão edit de ofertas de disciplinas

--- modulos/Seguranca/Database/Seeds/PermissoesAcademicoSeeder.php
+++ modulos/Seguranca/Database/Seeds/PermissoesAcademicoSeeder.php
@@ -470,6 +470,11 @@ class PermissoesAcademicoSeeder extends Seeder
         ]);
         $arrPermissoes[] = $permissao->prm_id;
 
+        $permissao = Permissao::create([
+            'prm_nome' => 'edit',
+            'prm_rota' => 'academico.ofertasdisciplinas.edit'
+        ]);
+        $arrPermissoes[] = $permissao->prm_id;
 
 
         //permissões do recurso matricularalunocurso
