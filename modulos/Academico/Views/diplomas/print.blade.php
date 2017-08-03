62ce99a78b1f344facd19854222b9b0c015ae0b5
PedroFellipe <pedrofellipemelo123@gmail.com>
2017-08-03 17:34:43 -0300

    ajustes no repository e na view de impressão de diplomas

--- modulos/Academico/Views/diplomas/print.blade.php
+++ modulos/Academico/Views/diplomas/print.blade.php
@@ -11,7 +11,7 @@
     .divesquerda {
       float: left;
       width: 350px;
-      height: 370px;
+      height: 360px;
       border: 0.4mm solid;
       border-top-style: none;
       border-bottom-style: none;
@@ -29,8 +29,8 @@
 @foreach($retorno as $dados)
     <br>
     <br>
-    <h4 style="font-family: Arial; margin-left:30px;font-weight: normal;"> EIXO: <b>{{$dados['EIXOCURSO']}} </b> </h4>
-    <h4 style="font-family: Arial; margin-left:30px;font-weight: normal;"> HABILITAÇÃO: <b>{{$dados['HABILITAÇÂO']}}</b> </h4>
+      <h4 style="font-family: Arial; margin-left:30px;font-weight: normal;"> EIXO: <b>{{$dados['EIXOCURSO']}} </b> </h4>
+      <h4 style="font-family: Arial; margin-left:30px;font-weight: normal;"> HABILITAÇÃO: <b>{{$dados['HABILITAÇÂO']}}</b> </h4>
     <br>
     <br>
     <!-- HTML -->
@@ -40,32 +40,32 @@
                 <p style="text-align:center"><b>UNIVERSIDADE ESTADUAL DO MARANHÃO</b><br>
                   <b>REGISTRO DE DIPLOMAS</b><br><br>
                   Registrado sob o N° <b>{{$dados['REGISTRO']}}</b><br><br>
-                  Livro N° <b>{{$dados['LIVRO']}}</b> FLS N °<b>{{$dados['FOLHA']}} </b><br><br>
-                  Processo N° <b>{{$dados['PROCESSO']}}</b><br><br><br><br>
+                  Livro N° <b>{{$dados['LIVRO']}}</b> FLS N° <b>{{$dados['FOLHA']}} </b><br><br>
+                  Processo N° <b>{{$dados['PROCESSO']}}</b><br><br><br><br><br>
                   <?php
                       setlocale(LC_ALL, 'pt_BR', 'pt_BR.utf-8', 'pt_BR.utf-8', 'portuguese');
                       date_default_timezone_set('America/Sao_Paulo');
                   ?>
                   SÃO LUÍS-MA, <u>{{strftime('%d', strtotime('today'))}}</u>/<u>{{strftime('%m', strtotime('today'))}}</u>/<u>{{strftime('%Y', strtotime('today'))}}</u><br><br><br><br><br>
                   _________________________________________________<br>
-                  <b>NOME DO PROFISSIONAL RESPONSÁVEL</b><br><br><br>
+                  <b>NOME DO PROFISSIONAL RESPONSÁVEL</b><br><br>
               </p>
               <b style="margin-left: 40px;font-size:9 px;">DIPLOMA VÁLIDO EM TODO TERRITÓRIO NACIONAL</b>
       </div>
 
       <div class="divdireita">
         <div style="padding: 20px 20px 0px 20px;border: 0.4mm solid;border-top-style:none;border-right-style: none;border-left-style: none;">
-          Observação: Curso de Educação Profissional Técnica de Nível Médio - Subsequente<br><br><br>
+                Observação: Curso de Educação Profissional Técnica de Nível Médio - Subsequente<br><br><br>
 
                  ASSINATURA ANVERSO:<br><br>
                  Nome do Diretor do Centro: <b>{{$dados['DIRETORCENTRO']}}</b><br>
-                 Matrícula: {{$dados['MATRICULADIRETORCENTRO']}}<br><br><br>
+                 Matrícula: <b>{{$dados['MATRICULADIRETORCENTRO']}}</b><br><br><br>
 
                  Nome do Diretor do Curso: <b>{{$dados['DIRETORCURSO']}}</b><br>
-                 Matrícula: {{$dados['MATRICULADIRETORCURSO']}}<br><br>
+                 Matrícula: <b>{{$dados['MATRICULADIRETORCURSO']}}</b><br><br>
         </div>
         <div style="padding: 20px 20px 0px 20px;">
-          N°. de Registro no SISTEC: <b>{{$dados['REGISTROSISTEC']}}</b><br><br><br>
+                N°. de Registro no SISTEC: <b>{{$dados['REGISTROSISTEC']}}</b><br><br><br>
                 CARGA HORÁRIA DA PRÁTICA PROFISSIONAL: <b>{{$dados['CHPRATICA']}}</b><br><br>
                 CARGA HORÁRIA TOTAL DO CURSO: <b>{{$dados['CHTOTAL']}}</b><br><br><br>
                 <b>Resolução de Reconhecimento N°</b>: {{$dados['RESOLUCAO']}} de {{strftime('%d de %B de %Y', $dados['DATA_AUTORIZACAO'])}}
@@ -76,13 +76,13 @@
 
     <pagebreak>
 
-        <br><br><br><br><br><br><br><br><br><br><br><br>
-        <p style="font-size:16pt; font-family: Arial;margin-left:0px; margin-right:50px; text-align:justify; line-height:1.5">
-          A UNIVERSIDADE ESTADUAL DO MARANHÃO, por meio do {{$dados['CENTRO']}}, tendo em vista a conclusão do <b>Curso Técnico de Nível Médio em {{$dados['CURSO']}}</b>, em {{strftime('%d de %B de %Y', $dados['CONCLUSAO'])}}, confere o título de {{$dados['HABILITAÇÂO']}} a <b>{{$dados['NOME']}}</b>, nascido(a) aos {{$dados['DIAEXTENSO']}} {{strftime('dias do mês de %B de %Y', $dados['NASCIMENTO'])}}, nacionalidade {{$dados['NACIONALIDADE']}}, natural de {{$dados['NATURALIDADE']}}, carteira de identidade N° {{$dados['IDENTIDADE']}} / {{$dados['ORGAO']}}, outorga-lhe o  presente Diploma a fim de que possa gozar de todos os direitos e prerrogativas legais.
+        <br><br><br><br><br><br><br><br><br><br><br><br><br>
+        <p style="font-size:14pt; font-family: Arial;margin-left:0px; margin-right:50px; text-align:justify; line-height:1.5">
+            A UNIVERSIDADE ESTADUAL DO MARANHÃO, por meio do {{$dados['CENTRO']}}, tendo em vista a conclusão do Curso Técnico de Nível Médio em {{$dados['CURSO']}}, em {{strftime('%d de %B de %Y', $dados['CONCLUSAO'])}}, confere o título de <b>{{$dados['HABILITAÇÂO']}}</b> a <b>{{$dados['NOME']}}</b>, nascido(a) aos {{$dados['DIAEXTENSO']}} {{strftime('dias do mês de %B de %Y', $dados['NASCIMENTO'])}}, nacionalidade {{$dados['NACIONALIDADE']}}, natural de {{$dados['NATURALIDADE']}}, carteira de identidade N° {{$dados['IDENTIDADE']}} / {{$dados['ORGAO']}}, outorga-lhe o  presente Diploma a fim de que possa gozar de todos os direitos e prerrogativas legais.
         </p>
 
         <p  style="font-size:18px; font-family: Arial;margin-left:0px; margin-right:50px; text-align:right">
-          São Luís (MA), {{strftime('%d de %B de %Y', strtotime('today'))}}<br>
+            São Luís (MA), {{strftime('%d de %B de %Y', strtotime('today'))}}<br>
         </p>
 
     </html>
