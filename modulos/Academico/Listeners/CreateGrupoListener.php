674f390b10d0e6479f2c3def8e94acc0c904cab2
Lucas S. Vieira <lucassouzavieiraengcomp@gmail.com>
2017-08-18 17:24:07 -0300

    Teste para listener de criação/migração de grupo

--- modulos/Academico/Listeners/CreateGrupoListener.php
+++ modulos/Academico/Listeners/CreateGrupoListener.php
@@ -3,6 +3,7 @@
 namespace Modulos\Academico\Listeners;
 
 use Moodle;
+use GuzzleHttp\Exception\ClientException;
 use GuzzleHttp\Exception\ConnectException;
 use Modulos\Academico\Events\CreateGrupoEvent;
 use Modulos\Integracao\Events\UpdateSincronizacaoEvent;
@@ -52,17 +53,23 @@ class CreateGrupoListener
                 event(new UpdateSincronizacaoEvent($grupo, $status, $response['message']));
             }
         } catch (ConnectException $exception) {
-            if (config('app.debug')) {
+            if (env('app.debug')) {
                 throw $exception;
             }
 
-            event(new UpdateSincronizacaoEvent($event->getData(), 3, $exception->getMessage()));
+            event(new UpdateSincronizacaoEvent($event->getData(), 3, $exception->getMessage(), $event->getAction()));
+        } catch (ClientException $exception) {
+            if (env('app.debug')) {
+                throw $exception;
+            }
+
+            event(new UpdateSincronizacaoEvent($event->getData(), 3, $exception->getMessage(), $event->getAction()));
         } catch (\Exception $exception) {
-            if (config('app.debug')) {
+            if (env('app.debug')) {
                 throw $exception;
             }
 
-            event(new UpdateSincronizacaoEvent($event->getData(), 3, $exception->getMessage()));
+            event(new UpdateSincronizacaoEvent($event->getData(), 3, $exception->getMessage(), $event->getAction()));
         } finally {
             return true;
         }
