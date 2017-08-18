7ba48eedf90ad8a2a86bb27ad132e6b350abd74a
Lucas S. Vieira <lucassouzavieiraengcomp@gmail.com>
2017-08-18 16:44:06 -0300

    Testes de listeners - modulo Geral

--- modulos/Geral/Listeners/UpdatePessoaListener.php
+++ modulos/Geral/Listeners/UpdatePessoaListener.php
@@ -3,6 +3,7 @@
 namespace Modulos\Geral\Listeners;
 
 use Moodle;
+use GuzzleHttp\Exception\ClientException;
 use GuzzleHttp\Exception\ConnectException;
 use Modulos\Geral\Events\UpdatePessoaEvent;
 use Modulos\Geral\Repositories\PessoaRepository;
@@ -73,19 +74,23 @@ class UpdatePessoaListener
                 ));
             }
         } catch (ConnectException $exception) {
-            if (config('app.debug')) {
+            if (env('app.debug')) {
+                throw $exception;
+            }
+
+            event(new UpdateSincronizacaoEvent($event->getData(), 3, $exception->getMessage(), $event->getAction()));
+        } catch (ClientException $exception) {
+            if (env('app.debug')) {
                 throw $exception;
             }
 
             event(new UpdateSincronizacaoEvent($event->getData(), 3, $exception->getMessage(), $event->getAction()));
         } catch (\Exception $exception) {
-            if (config('app.debug')) {
+            if (env('app.debug')) {
                 throw $exception;
             }
 
             event(new UpdateSincronizacaoEvent($event->getData(), 3, $exception->getMessage(), $event->getAction()));
-        } finally {
-            return true;
         }
     }
 }
