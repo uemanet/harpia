609b0cd382b96233ad5302c4bdd71abe7b1f736c
PedroFellipe <pedrofellipemelo123@gmail.com>
2017-08-21 13:57:12 -0300

    botão para migrar todos os erros da tabela de sincronização criado

--- modulos/Integracao/Views/sincronizacao/index.blade.php
+++ modulos/Integracao/Views/sincronizacao/index.blade.php
@@ -43,8 +43,17 @@
         <!-- /.box-body -->
     </div>
     @if(!is_null($tabela))
-        <div class="box box-primary">
+          <div class="box box-primary">
             <div class="box-header">
+              <div class="row">
+                <div class="col-md-3 pull-right">
+                  <div class="form-group">
+                    <button class="btn btn-success form-control btn-mapear">
+                      <i class="fa fa-exchange"></i> Migrar Todos os erros
+                    </button>
+                  </div>
+                </div>
+              </div>
                 {!! $tabela->render() !!}
             </div>
         </div>
@@ -56,4 +65,71 @@
             <div class="box-body">Sem registros para apresentar</div>
         </div>
     @endif
+@stop
+
+@section('scripts')
+    <script>
+        $(function() {
+            $(document).on('click', '.btn-mapear', function (event) {
+
+              //Pega todas as linhas da tabela que estão com status de falha
+              itens = new Array();
+              $('table tbody tr').each(function () {
+
+                  var colunas = $(this).children();
+                  var item = {
+                      'id': trim($(colunas[0]).text()),
+                      'status': trim($(colunas[3]).text())
+
+                  };
+                  // Adicionar o objeto item no array
+                  if (item.status === "Falha") {
+                    itens.push(item.id);
+                  }
+              });
+
+              // listando os items (teste)
+              console.info(itens);
+
+              var dados = {
+                ids: itens,
+                _token: "{{csrf_token()}}"
+              };
+
+              var result = false;
+              $.harpia.showloading();
+
+              $.ajax({
+                type: 'POST',
+                url: '/integracao/async/sincronizacao/sincronizar',
+                data: dados,
+                success: function (response) {
+                  $.harpia.hideloading();
+
+                  toastr.success('Sincronização efetuada com sucesso!', null, {progressBar: true});
+
+                },
+                error: function (xhr, textStatus, error) {
+                  $.harpia.hideloading();
+
+                  switch (xhr.status) {
+                    case 400:
+                    toastr.error(xhr.responseText.replace(/\"/g, ''), null, {progressBar: true});
+                    break;
+                    default:
+                    toastr.error(xhr.responseText.replace(/\"/g, ''), null, {progressBar: true});
+
+                    result = false;
+                  }
+                }
+              });
+
+            });
+        });
+
+        function trim(str) {
+            return str.replace(/^\s+|\s+$/g,"");
+        }
+
+    </script>
 @stop
