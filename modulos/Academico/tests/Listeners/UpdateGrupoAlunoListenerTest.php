343ca9cbac5bf55e1c0d644c3e4d71e8e581f823
Lucas S. Vieira <lucassouzavieiraengcomp@gmail.com>
2017-08-22 19:26:12 -0300

    Testes de atualização de grupo de aluno

--- modulos/Academico/tests/Listeners/UpdateGrupoAlunoListenerTest.php
+++ modulos/Academico/tests/Listeners/UpdateGrupoAlunoListenerTest.php
@@ -1,7 +1,198 @@
 <?php
 
-use Modulos\Academico\Listeners\UpdateGrupoAlunoListener;
+use Modulos\Academico\Events\CreateGrupoEvent;
+use Modulos\Integracao\Events\TurmaMapeadaEvent;
+use Modulos\Academico\Events\UpdateGrupoAlunoEvent;
+use Modulos\Academico\Events\CreateMatriculaTurmaEvent;
 
-class UpdateGrupoAlunoListenerTest extends PHPUnit_Framework_TestCase
+class UpdateGrupoAlunoListenerTest extends TestCase
 {
+    protected $ambiente;
+    protected $sincronizacaoRepository;
+    protected $turma;
+    protected $grupo;
+    protected $matricula;
+
+    public function createApplication()
+    {
+        putenv('DB_CONNECTION=sqlite_testing');
+
+        $app = require __DIR__ . '/../../../../bootstrap/app.php';
+
+        $app->make(\Illuminate\Contracts\Console\Kernel::class)->bootstrap();
+
+        return $app;
+    }
+
+    public function setUp()
+    {
+        parent::setUp();
+
+        Artisan::call('modulos:migrate');
+
+        $this->sincronizacaoRepository = $this->app->make(\Modulos\Integracao\Repositories\SincronizacaoRepository::class);
+
+        Modulos\Integracao\Models\Servico::truncate();
+
+        $this->createAmbiente();
+        $this->createIntegracao();
+        $this->createMonitor();
+        $this->mockUpDatabase();
+    }
+
+    /**
+     * Cria um ambiente de testes a partir das variaveis de ambiente
+     *
+     * @return void
+     */
+    private function createAmbiente()
+    {
+        $moodle = [
+            'amb_nome' => 'Moodle',
+            'amb_versao' => '3.2+',
+            'amb_url' => "http://localhost:8080"
+        ];
+
+        $this->ambiente = factory(Modulos\Integracao\Models\AmbienteVirtual::class)->create($moodle);
+    }
+
+    /**
+     * Cria o registro do plugin de integracao a partir das variaveis de ambiente
+     *
+     * @return void
+     */
+    private function createIntegracao()
+    {
+        $servico = factory(Modulos\Integracao\Models\Servico::class)->create([
+            'ser_id' => 2,
+            'ser_nome' => "Integração",
+            'ser_slug' => "local_integracao"
+        ]);
+
+        $ambienteServico = factory(\Modulos\Integracao\Models\AmbienteServico::class)->create([
+            'asr_amb_id' => $this->ambiente->amb_id,
+            'asr_ser_id' => $servico->ser_id,
+            'asr_token' => env("MOODLE_INTEGRACAO_TEST_TOKEN")
+        ]);
+    }
+
+    /**
+     * Cria o registro do plugin de monitoramento a partir das variaveis de ambiente
+     *
+     * @return void
+     */
+    private function createMonitor()
+    {
+        $servico = factory(Modulos\Integracao\Models\Servico::class)->create([
+            'ser_nome' => "Monitor",
+            'ser_slug' => "get_tutor_online_time"
+        ]);
+
+        $ambienteServico = factory(\Modulos\Integracao\Models\AmbienteServico::class)->create([
+            'asr_amb_id' => $this->ambiente->amb_id,
+            'asr_ser_id' => $servico->ser_id,
+            'asr_token' => "abcdefgh12345"
+        ]);
+    }
+
+
+    /**
+     * Fazer mock-up de todas as etapas necessarias para se mapear uma turma em um ambiente virtual
+     *
+     * @return void
+     */
+    private function mockUpDatabase()
+    {
+        // Cria a turma
+        $data = [
+            'trm_id' => random_int(50, 100),
+            'trm_ofc_id' => factory(Modulos\Academico\Models\OfertaCurso::class)->create()->ofc_id,
+            'trm_per_id' => factory(Modulos\Academico\Models\PeriodoLetivo::class)->create()->per_id,
+            'trm_nome' => "Turma de Teste",
+            'trm_integrada' => 1,
+            'trm_qtd_vagas' => 50
+        ];
+
+        $this->turma = factory(Modulos\Academico\Models\Turma::class)->create($data);
+
+        // Vincular com o ambiente
+        factory(\Modulos\Integracao\Models\AmbienteTurma::class)->create([
+            'atr_trm_id' => $this->turma->trm_id,
+            'atr_amb_id' => $this->ambiente->amb_id
+        ]);
+
+        // Mapeia a turma
+        $sincronizacaoListener = $this->app->make(\Modulos\Integracao\Listeners\SincronizacaoListener::class);
+        $turmaMapeadaListener = $this->app->make(\Modulos\Integracao\Listeners\TurmaMapeadaListener::class);
+        $createGroupListener = $this->app->make(\Modulos\Academico\Listeners\CreateGrupoListener::class);
+
+        // Eventos de turma
+        $turmaMapeadaEvent = new TurmaMapeadaEvent($this->turma);
+
+        $sincronizacaoListener->handle($turmaMapeadaEvent);
+        $turmaMapeadaListener->handle($turmaMapeadaEvent);
+
+        // Cria o grupo
+        $this->grupo = factory(\Modulos\Academico\Models\Grupo::class)->create([
+            'grp_trm_id' => $this->turma->trm_id,
+            'grp_pol_id' => factory(Modulos\Academico\Models\Polo::class)->create()->pol_id,
+            'grp_nome' => "Group A"
+        ]);
+
+        // Eventos de grupo
+        $createGroupEvent = new CreateGrupoEvent($this->grupo);
+
+        $sincronizacaoListener->handle($createGroupEvent);
+        $createGroupListener->handle($createGroupEvent);
+
+        // Cria a matricula
+        $this->matricula = factory(\Modulos\Academico\Models\Matricula::class)->create([
+            'mat_trm_id' => $this->turma->trm_id,
+            'mat_grp_id' => $this->grupo->grp_id,
+        ]);
+    }
+
+    public function testHandle()
+    {
+        $sincronizacaoListener = $this->app->make(\Modulos\Integracao\Listeners\SincronizacaoListener::class);
+        $updateGrupoAlunoListener = $this->app->make(\Modulos\Academico\Listeners\UpdateGrupoAlunoListener::class);
+
+        $matriculaRepository = $this->app->make(\Modulos\Academico\Repositories\MatriculaCursoRepository::class);
+        $grupoRepository = $this->app->make(\Modulos\Academico\Repositories\GrupoRepository::class);
+
+        $this->assertEquals(2, $this->sincronizacaoRepository->count());
+
+        // Cria novo grupo
+        $novoGrupo = factory(\Modulos\Academico\Models\Grupo::class)->create([
+            'grp_trm_id' => $this->turma->trm_id,
+            'grp_pol_id' => factory(Modulos\Academico\Models\Polo::class)->create()->pol_id,
+            'grp_nome' => "Group B"
+        ]);
+
+        // Atualiza a matricula para novo grupo
+        $oldGrupo = $this->matricula->mat_grp_id;
+
+        $matriculaRepository->update([
+            'mat_grp_id' => $novoGrupo->grp_id
+        ], $this->matricula->mat_id);
+
+        // Dispara evento de atualizacao
+        $updateGrupoAlunoEvent = new UpdateGrupoAlunoEvent($this->matricula, $oldGrupo);
+        $sincronizacaoListener->handle($updateGrupoAlunoEvent);
+
+        $this->seeInDatabase('int_sync_moodle', [
+            'sym_table' => $updateGrupoAlunoEvent->getData()->getTable(),
+            'sym_table_id' => $updateGrupoAlunoEvent->getData()->getKey(),
+            'sym_action' => $updateGrupoAlunoEvent->getAction(),
+            'sym_status' => 1,
+            'sym_mensagem' => null,
+            'sym_data_envio' => null,
+            'sym_extra' => $updateGrupoAlunoEvent->getExtra()
+        ]);
+
+        $this->expectsEvents(\Modulos\Integracao\Events\UpdateSincronizacaoEvent::class);
+        $updateGrupoAlunoListener->handle($updateGrupoAlunoEvent);
+
+        $this->assertEquals(3, $this->sincronizacaoRepository->count());
+    }
 }
