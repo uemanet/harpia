77ffdd6a245d05ed9cd9ab97164ff3950a5efe6e
Lucas S. Vieira <lucassouzavieiraengcomp@gmail.com>
2017-10-13 17:52:12 -0300

    Index

---
+++ modulos/Academico/Http/Controllers/Async/LancamentoNotas.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Modulos\Academico\Http\Controllers\Async;
+
+use Modulos\Core\Http\Controller\BaseController;
+
+class LancamentoNotas extends BaseController
+{
+    public function __construct()
+    {
+    }
+
+    public function getIndex()
+    {
+        return "Nothing";
+    }
+}
