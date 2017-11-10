3446b0e15543985051ca38877fe3ceae4ed03a84
Felipe Pimenta <fhpimenta12@gmail.com>
2017-11-10 15:08:10 -0300

    Migration de alteração do campo mdo_competencias, da tabela acd_modulos_matrizes, para o tipo text

---
+++ modulos/Academico/Database/Migrations/2017_11_10_160044_alter_field_mdo_competencias_modulos_matrizes.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AlterFieldMdoCompetenciasModulosMatrizes extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('acd_modulos_matrizes', function (Blueprint $table) {
+            $table->text('mdo_competencias')->nullable()->change();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('acd_modulos_matrizes', function (Blueprint $table) {
+            $table->string('mdo_competencias', 255)->nullable();
+        });
+    }
+}
