dc4f118eaa90b68e66ce3e38031158ae9cdfc712
Lucas S. Vieira <lucassouzavieiraengcomp@gmail.com>
2017-11-22 14:17:12 -0300

    Ambiente virtual repository testes

--- modulos/Integracao/tests/Repositories/AmbienteVirtualRepositoryTest.php
+++ modulos/Integracao/tests/Repositories/AmbienteVirtualRepositoryTest.php
@@ -1,61 +1,155 @@
 <?php
 
-use Illuminate\Foundation\Testing\WithoutMiddleware;
-use Illuminate\Foundation\Testing\DatabaseTransactions;
-use Modulos\Integracao\Repositories\AmbienteVirtualRepository;
+use Tests\ModulosTestCase;
+use Modulos\Integracao\Models\AmbienteVirtual;
+use Stevebauman\EloquentTable\TableCollection;
 use Illuminate\Pagination\LengthAwarePaginator;
-use Illuminate\Database\Eloquent\Collection;
-use Illuminate\Support\Facades\Artisan;
+use Modulos\Integracao\Repositories\AmbienteVirtualRepository;
 
-class AmbienteVirtualRepositoryTest extends TestCase
+class AmbienteVirtualRepositoryTest extends ModulosTestCase
 {
-    use DatabaseTransactions,
-        WithoutMiddleware;
-
-    protected $repo;
-
-    public function createApplication()
-    {
-        putenv('DB_CONNECTION=sqlite_testing');
-
-        $app = require __DIR__ . '/../../../../bootstrap/app.php';
-
-        $app->make('Illuminate\Contracts\Console\Kernel')->bootstrap();
-
-        return $app;
-    }
-
     public function setUp()
     {
         parent::setUp();
-
-        Artisan::call('modulos:migrate');
-
         $this->repo = $this->app->make(AmbienteVirtualRepository::class);
+        $this->table = 'int_ambientes_virtuais';
     }
 
-    public function testAllWithEmptyDatabase()
+    public function testCreate()
     {
-        $response = $this->repo->all();
+        $data = factory(AmbienteVirtual::class)->raw();
+        $entry = $this->repo->create($data);
 
-        $this->assertInstanceOf(Collection::class, $response);
-        $this->assertEquals(0, $response->count());
+        $this->assertInstanceOf(AmbienteVirtual::class, $entry);
+        $this->assertDatabaseHas($this->table, $entry->toArray());
+    }
+
+    public function testFind()
+    {
+        $entry = factory(AmbienteVirtual::class)->create();
+        $id = $entry->amb_id;
+        $fromRepository = $this->repo->find($id);
+
+        $this->assertInstanceOf(AmbienteVirtual::class, $fromRepository);
+        $this->assertDatabaseHas($this->table, $fromRepository->toArray());
+        $this->assertEquals($entry->toArray(), $fromRepository->toArray());
+    }
+
+    public function testUpdate()
+    {
+        $entry = factory(AmbienteVirtual::class)->create();
+        $id = $entry->amb_id;
+
+        $data = $entry->toArray();
+
+        $data['amb_nome'] = "New name";
+
+        $return = $this->repo->update($data, $id);
+        $fromRepository = $this->repo->find($id);
+
+        $this->assertEquals(1, $return);
+        $this->assertDatabaseHas($this->table, $data);
+        $this->assertInstanceOf(AmbienteVirtual::class, $fromRepository);
+        $this->assertEquals($data, $fromRepository->toArray());
+    }
+
+    public function testDelete()
+    {
+        $entry = factory(AmbienteVirtual::class)->create();
+        $id = $entry->amb_id;
+
+        $return = $this->repo->delete($id);
+
+        $this->assertEquals(1, $return);
+        $this->assertDatabaseMissing($this->table, $entry->toArray());
+    }
+
+    public function testLists()
+    {
+        $entries = factory(AmbienteVirtual::class, 2)->create();
+
+        $model = new AmbienteVirtual();
+        $expected = $model->pluck('amb_nome', 'amb_id');
+        $fromRepository = $this->repo->lists('amb_id', 'amb_nome');
+
+        $this->assertEquals($expected, $fromRepository);
+    }
+
+    public function testSearch()
+    {
+        $entries = factory(AmbienteVirtual::class, 2)->create();
+
+        factory(AmbienteVirtual::class)->create([
+            'amb_versao' => "3.1"
+        ]);
+
+        $searchResult = $this->repo->search(array(['amb_versao', '=', "3.1"]));
+
+        $this->assertInstanceOf(TableCollection::class, $searchResult);
+        $this->assertEquals(1, $searchResult->count());
+    }
+
+    public function testSearchWithSelect()
+    {
+        factory(AmbienteVirtual::class, 2)->create();
+
+        $entry = factory(AmbienteVirtual::class)->create([
+            'amb_nome' => "New name"
+        ]);
+
+        $expected = [
+            'amb_id' => $entry->amb_id,
+            'amb_nome' => $entry->amb_nome
+        ];
+
+        $searchResult = $this->repo->search(array(['amb_nome', '=', "New name"]), ['amb_id', 'amb_nome']);
+
+        $this->assertInstanceOf(TableCollection::class, $searchResult);
+        $this->assertEquals(1, $searchResult->count());
+        $this->assertEquals($expected, $searchResult->first()->toArray());
+    }
+
+    public function testAll()
+    {
+        // With empty database
+        $collection = $this->repo->all();
+
+        $this->assertEquals(0, $collection->count());
+
+        // Non-empty database
+        $created = factory(AmbienteVirtual::class, 10)->create();
+        $collection = $this->repo->all();
+
+        $this->assertEquals($created->count(), $collection->count());
+    }
+
+    public function testCount()
+    {
+        $created = factory(AmbienteVirtual::class, 10)->create();
+        $collection = $this->repo->all();
+
+        $this->assertEquals($created->count(), $this->repo->count());
+    }
+
+    public function testGetFillableModelFields()
+    {
+        $model = new AmbienteVirtual();
+        $this->assertEquals($model->getFillable(), $this->repo->getFillableModelFields());
     }
 
     public function testPaginateWithoutParameters()
     {
-        factory(Modulos\Integracao\Models\AmbienteVirtual::class, 2)->create();
+        factory(AmbienteVirtual::class, 2)->create();
 
         $response = $this->repo->paginate();
 
         $this->assertInstanceOf(LengthAwarePaginator::class, $response);
-
         $this->assertGreaterThan(1, $response->total());
     }
 
     public function testPaginateWithSort()
     {
-        factory(Modulos\Integracao\Models\AmbienteVirtual::class, 2)->create();
+        factory(AmbienteVirtual::class, 2)->create();
 
         $sort = [
             'field' => 'amb_id',
@@ -65,64 +159,33 @@ class AmbienteVirtualRepositoryTest extends TestCase
         $response = $this->repo->paginate($sort);
 
         $this->assertInstanceOf(LengthAwarePaginator::class, $response);
-
-        $this->assertEquals(2, $response[0]->amb_id);
+        $this->assertEquals(2, $response->first()->amb_id);
     }
 
     public function testPaginateWithSearch()
     {
-        factory(Modulos\Integracao\Models\AmbienteVirtual::class, 2)->create();
-
-        factory(Modulos\Integracao\Models\AmbienteVirtual::class)->create([
-            'amb_nome' => 'icatu',
+        factory(AmbienteVirtual::class, 2)->create();
+        factory(AmbienteVirtual::class)->create([
+            'amb_nome' => 1,
         ]);
 
         $search = [
             [
                 'field' => 'amb_nome',
-                'type' => 'like',
-                'term' => 'icatu'
+                'type' => '=',
+                'term' => 1
             ]
         ];
 
         $response = $this->repo->paginate(null, $search);
-
         $this->assertInstanceOf(LengthAwarePaginator::class, $response);
-
         $this->assertGreaterThan(0, $response->total());
-
-        $this->assertEquals('icatu', $response[0]->amb_nome);
-    }
-
-    public function testPaginateWithSearchAndOrder()
-    {
-        factory(Modulos\Integracao\Models\AmbienteVirtual::class, 2)->create();
-
-        $sort = [
-            'field' => 'amb_id',
-            'sort' => 'desc'
-        ];
-
-        $search = [
-            [
-                'field' => 'amb_id',
-                'type' => '>',
-                'term' => '1'
-            ]
-        ];
-
-        $response = $this->repo->paginate($sort, $search);
-
-        $this->assertInstanceOf(LengthAwarePaginator::class, $response);
-
-        $this->assertGreaterThan(0, $response->total());
-
-        $this->assertEquals(2, $response[0]->amb_id);
+        $this->assertEquals(1, $response->first()->amb_nome);
     }
 
     public function testPaginateRequest()
     {
-        factory(Modulos\Integracao\Models\AmbienteVirtual::class, 2)->create();
+        factory(AmbienteVirtual::class, 2)->create();
 
         $requestParameters = [
             'page' => '1',
@@ -131,56 +194,7 @@ class AmbienteVirtualRepositoryTest extends TestCase
         ];
 
         $response = $this->repo->paginateRequest($requestParameters);
-
         $this->assertInstanceOf(LengthAwarePaginator::class, $response);
-
         $this->assertGreaterThan(0, $response->total());
-    }
-
-    public function testCreate()
-    {
-        $response = factory(Modulos\Integracao\Models\AmbienteVirtual::class)->create();
-
-        $this->assertInstanceOf(\Modulos\Integracao\Models\AmbienteVirtual::class, $response);
-
-        $this->assertArrayHasKey('amb_id', $response->toArray());
-    }
-
-    public function testFind()
-    {
-        $data = factory(Modulos\Integracao\Models\AmbienteVirtual::class)->create();
-
-        $this->assertDatabaseHas('int_ambientes_virtuais', $data->toArray());
-    }
-
-    public function testUpdate()
-    {
-        $data = factory(Modulos\Integracao\Models\AmbienteVirtual::class)->create();
-
-        $updateArray = $data->toArray();
-        $updateArray['amb_nome'] = 'abcde_edcba';
-
-        $ambienteVirtualId = $updateArray['amb_id'];
-        unset($updateArray['amb_id']);
-
-        $response = $this->repo->update($updateArray, $ambienteVirtualId, 'amb_id');
-
-        $this->assertEquals(1, $response);
-    }
-
-    public function testDelete()
-    {
-        $data = factory(Modulos\Integracao\Models\AmbienteVirtual::class)->create();
-        $ambienteVirtualId = $data->amb_id;
-
-        $response = $this->repo->delete($ambienteVirtualId);
-
-        $this->assertEquals(1, $response);
-    }
-
-    public function tearDown()
-    {
-        Artisan::call('migrate:reset');
-        parent::tearDown();
     }
 }
