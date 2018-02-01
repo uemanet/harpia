5d938e5483144c8f8ce1a2c545835e7041d6056a
Pedro Fellipe <pedrofellipemelo123@gmail.com>
2018-02-01 16:04:46 -0300

    repositório de Modalidade refatorado

--- modulos/Academico/tests/Repositories/ModalidadeRepositoryTest.php
+++ modulos/Academico/tests/Repositories/ModalidadeRepositoryTest.php
@@ -1,46 +1,140 @@
 <?php
 
-use Illuminate\Foundation\Testing\WithoutMiddleware;
-use Illuminate\Foundation\Testing\DatabaseTransactions;
-use Modulos\Academico\Repositories\ModalidadeRepository;
+use Tests\ModulosTestCase;
 use Modulos\Academico\Models\Modalidade;
+use Stevebauman\EloquentTable\TableCollection;
 use Illuminate\Pagination\LengthAwarePaginator;
-use Illuminate\Database\Eloquent\Collection;
-use Illuminate\Support\Facades\Artisan;
+use Modulos\Academico\Repositories\ModalidadeRepository;
 
-class ModalidadeRepositoryTest extends TestCase
+class ModalidadeRepositoryTest extends ModulosTestCase
 {
-    use DatabaseTransactions,
-        WithoutMiddleware;
-
-    protected $repo;
-
-    public function createApplication()
-    {
-        putenv('DB_CONNECTION=sqlite_testing');
-
-        $app = require __DIR__ . '/../../../../bootstrap/app.php';
-
-        $app->make('Illuminate\Contracts\Console\Kernel')->bootstrap();
-
-        return $app;
-    }
-
     public function setUp()
     {
         parent::setUp();
-
-        Artisan::call('modulos:migrate');
-
         $this->repo = $this->app->make(ModalidadeRepository::class);
+        $this->table = 'acd_modalidades';
     }
 
-    public function testAllWithEmptyDatabase()
+    public function testCreate()
     {
-        $response = $this->repo->all();
+        $data = factory(Modalidade::class)->raw();
+        $entry = $this->repo->create($data);
 
-        $this->assertInstanceOf(Collection::class, $response);
-        $this->assertEquals(0, $response->count());
+        $this->assertInstanceOf(Modalidade::class, $entry);
+        $this->assertDatabaseHas($this->table, $entry->toArray());
+    }
+
+    public function testFind()
+    {
+        $entry = factory(Modalidade::class)->create();
+        $id = $entry->mdl_id;
+        $fromRepository = $this->repo->find($id);
+
+        $this->assertInstanceOf(Modalidade::class, $fromRepository);
+        $this->assertDatabaseHas($this->table, $fromRepository->toArray());
+        $this->assertEquals($entry->toArray(), $fromRepository->toArray());
+    }
+
+    public function testUpdate()
+    {
+        $entry = factory(Modalidade::class)->create();
+        $id = $entry->mdl_id;
+
+        $data = $entry->toArray();
+
+        $data['mdl_nome'] = "slug";
+
+        $return = $this->repo->update($data, $id);
+        $fromRepository = $this->repo->find($id);
+
+        $this->assertEquals(1, $return);
+        $this->assertDatabaseHas($this->table, $data);
+        $this->assertInstanceOf(Modalidade::class, $fromRepository);
+        $this->assertEquals($data, $fromRepository->toArray());
+    }
+
+    public function testDelete()
+    {
+        $entry = factory(Modalidade::class)->create();
+        $id = $entry->mdl_id;
+
+        $return = $this->repo->delete($id);
+
+        $this->assertEquals(1, $return);
+        $this->assertDatabaseMissing($this->table, $entry->toArray());
+    }
+
+    public function testLists()
+    {
+        $entries = factory(Modalidade::class, 2)->create();
+
+        $model = new Modalidade();
+        $expected = $model->pluck('mdl_nome', 'mdl_id');
+        $fromRepository = $this->repo->lists('mdl_id', 'mdl_nome');
+
+        $this->assertEquals($expected, $fromRepository);
+    }
+
+    public function testSearch()
+    {
+        $entries = factory(Modalidade::class, 2)->create();
+
+        factory(Modalidade::class)->create([
+            'mdl_nome' => 'mdlartamento'
+        ]);
+
+        $searchResult = $this->repo->search(array(['mdl_nome', '=', 'mdlartamento']));
+
+        $this->assertInstanceOf(TableCollection::class, $searchResult);
+        $this->assertEquals(1, $searchResult->count());
+    }
+
+    public function testSearchWithSelect()
+    {
+        factory(Modalidade::class, 2)->create();
+
+        $entry = factory(Modalidade::class)->create([
+            'mdl_nome' => "mdlartamento"
+        ]);
+
+        $expected = [
+            'mdl_id' => $entry->mdl_id,
+            'mdl_nome' => $entry->mdl_nome
+        ];
+
+        $searchResult = $this->repo->search(array(['mdl_nome', '=', "mdlartamento"]), ['mdl_id', 'mdl_nome']);
+
+        $this->assertInstanceOf(TableCollection::class, $searchResult);
+        $this->assertEquals(1, $searchResult->count());
+        $this->assertEquals($expected, $searchResult->first()->toArray());
+    }
+
+    public function testAll()
+    {
+        // With empty database
+        $collection = $this->repo->all();
+
+        $this->assertEquals(0, $collection->count());
+
+        // Non-empty database
+        $created = factory(Modalidade::class, 10)->create();
+        $collection = $this->repo->all();
+
+        $this->assertEquals($created->count(), $collection->count());
+    }
+
+    public function testCount()
+    {
+        $created = factory(Modalidade::class, 10)->create();
+        $collection = $this->repo->all();
+
+        $this->assertEquals($created->count(), $this->repo->count());
+    }
+
+    public function testGetFillableModelFields()
+    {
+        $model = new Modalidade();
+        $this->assertEquals($model->getFillable(), $this->repo->getFillableModelFields());
     }
 
     public function testPaginateWithoutParameters()
@@ -50,7 +144,6 @@ class ModalidadeRepositoryTest extends TestCase
         $response = $this->repo->paginate();
 
         $this->assertInstanceOf(LengthAwarePaginator::class, $response);
-
         $this->assertGreaterThan(1, $response->total());
     }
 
@@ -66,55 +159,28 @@ class ModalidadeRepositoryTest extends TestCase
         $response = $this->repo->paginate($sort);
 
         $this->assertInstanceOf(LengthAwarePaginator::class, $response);
-
-        $this->assertGreaterThan(1, $response[0]->mdl_id);
+        $this->assertEquals(2, $response->first()->mdl_id);
     }
 
     public function testPaginateWithSearch()
     {
         factory(Modalidade::class, 2)->create();
-
         factory(Modalidade::class)->create([
-            'mdl_nome' => 'agronomia',
+            'mdl_nome' => 'mdlartamento',
         ]);
 
         $search = [
             [
                 'field' => 'mdl_nome',
-                'type' => 'like',
-                'term' => 'agronomia'
+                'type' => '=',
+                'term' => 'mdlartamento'
             ]
         ];
 
         $response = $this->repo->paginate(null, $search);
-
         $this->assertInstanceOf(LengthAwarePaginator::class, $response);
-
-        $this->assertCount(1, $response);
-    }
-
-    public function testPaginateWithSearchAndOrder()
-    {
-        factory(Modalidade::class, 2)->create();
-
-        $sort = [
-            'field' => 'mdl_id',
-            'sort' => 'desc'
-        ];
-
-        $search = [
-            [
-                'field' => 'mdl_id',
-                'type' => '>',
-                'term' => '1'
-            ]
-        ];
-
-        $response = $this->repo->paginate($sort, $search);
-
-        $this->assertInstanceOf(LengthAwarePaginator::class, $response);
-
         $this->assertGreaterThan(0, $response->total());
+        $this->assertEquals('mdlartamento', $response->first()->mdl_nome);
     }
 
     public function testPaginateRequest()
@@ -128,51 +194,7 @@ class ModalidadeRepositoryTest extends TestCase
         ];
 
         $response = $this->repo->paginateRequest($requestParameters);
-
         $this->assertInstanceOf(LengthAwarePaginator::class, $response);
-
         $this->assertGreaterThan(0, $response->total());
-    }
-
-    public function testCreate()
-    {
-        $response = factory(Modalidade::class)->create();
-
-        $data = $response->toArray();
-
-        $this->assertInstanceOf(Modalidade::class, $response);
-
-        $this->assertArrayHasKey('mdl_id', $data);
-    }
-
-    public function testUpdate()
-    {
-        $data = factory(Modalidade::class)->create();
-
-        $updateArray = $data->toArray();
-        $updateArray['mdl_nome'] = 'abcde_edcba';
-
-        $centroId = $updateArray['mdl_id'];
-        unset($updateArray['mdl_id']);
-
-        $response = $this->repo->update($updateArray, $centroId, 'mdl_id');
-
-        $this->assertEquals(1, $response);
-    }
-
-    public function testDelete()
-    {
-        $data = factory(Modalidade::class)->create();
-        $id = $data->mdl_id;
-
-        $response = $this->repo->delete($id);
-
-        $this->assertEquals(1, $response);
-    }
-
-    public function tearDown()
-    {
-        Artisan::call('migrate:reset');
-        parent::tearDown();
     }
 }
