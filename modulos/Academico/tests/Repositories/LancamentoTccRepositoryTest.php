c26ef35665a65e2e388e557e9c498e5bf0ba4a13
Pedro Fellipe <pedrofellipemelo123@gmail.com>
2018-02-01 14:26:03 -0300

    testes no repositório de lançamento de Tcc refatorados

--- modulos/Academico/tests/Repositories/LancamentoTccRepositoryTest.php
+++ modulos/Academico/tests/Repositories/LancamentoTccRepositoryTest.php
@@ -1,68 +1,168 @@
 <?php
 
-use Illuminate\Foundation\Testing\WithoutMiddleware;
-use Illuminate\Foundation\Testing\DatabaseTransactions;
+use Tests\ModulosTestCase;
+use Modulos\Academico\Models\LancamentoTcc;
+use Stevebauman\EloquentTable\TableCollection;
+use Illuminate\Pagination\LengthAwarePaginator;
 use Modulos\Academico\Repositories\LancamentoTccRepository;
 use Modulos\Academico\Repositories\ModuloDisciplinaRepository;
-use Modulos\Academico\Repositories\TurmaRepository;
-use Illuminate\Pagination\LengthAwarePaginator;
-use Illuminate\Database\Eloquent\Collection;
-use Illuminate\Support\Facades\Artisan;
-use Carbon\Carbon;
 
-class LancamentoTccRepositoryTest extends TestCase
+class LancamentoTccRepositoryTest extends ModulosTestCase
 {
-    use DatabaseTransactions,
-        WithoutMiddleware;
-
-    protected $repo;
     protected $modulodisciplinaRepository;
-    protected $turmaRepository;
-
-    public function createApplication()
-    {
-        putenv('DB_CONNECTION=sqlite_testing');
-
-        $app = require __DIR__ . '/../../../../bootstrap/app.php';
-
-        $app->make('Illuminate\Contracts\Console\Kernel')->bootstrap();
-
-        return $app;
-    }
 
     public function setUp()
     {
         parent::setUp();
-
-        Artisan::call('modulos:migrate');
-
         $this->repo = $this->app->make(LancamentoTccRepository::class);
         $this->modulodisciplinaRepository = $this->app->make(ModuloDisciplinaRepository::class);
-        $this->turmaRepository = $this->app->make(TurmaRepository::class);
+        $this->table = 'acd_lancamentos_tccs';
     }
 
-    public function testAllWithEmptyDatabase()
+    public function testCreate()
     {
-        $response = $this->repo->all();
+        $data = factory(LancamentoTcc::class)->raw();
+        $entry = $this->repo->create($data);
 
-        $this->assertInstanceOf(Collection::class, $response);
-        $this->assertEquals(0, $response->count());
+        $this->assertInstanceOf(LancamentoTcc::class, $entry);
+        $entryData = $entry->toArray();
+        $entryData['ltc_data_apresentacao'] = $entry->getOriginal('ltc_data_apresentacao');
+        $this->assertDatabaseHas($this->table, $entryData);
+    }
+
+    public function testFind()
+    {
+        $entry = factory(LancamentoTcc::class)->create();
+        $id = $entry->ltc_id;
+        $fromRepository = $this->repo->find($id);
+
+        $entryData = $entry->toArray();
+        $entryData['ltc_data_apresentacao'] = $entry->getOriginal('ltc_data_apresentacao');
+
+        $fromRepositoryData = $fromRepository->toArray();
+        $fromRepositoryData['ltc_data_apresentacao'] = $fromRepository->getOriginal('ltc_data_apresentacao');
+
+        $this->assertInstanceOf(LancamentoTcc::class, $fromRepository);
+        $this->assertDatabaseHas($this->table, $fromRepositoryData);
+        $this->assertEquals($entryData, $fromRepositoryData);
+    }
+
+    public function testUpdate()
+    {
+        $entry = factory(LancamentoTcc::class)->create();
+        $id = $entry->ltc_id;
+
+        $data = $entry->toArray();
+
+        $data['ltc_titulo'] = "slug";
+
+        $return = $this->repo->update($data, $id);
+        $fromRepository = $this->repo->find($id);
+        $data['ltc_data_apresentacao'] = $entry->getOriginal('ltc_data_apresentacao');
+
+        $this->assertEquals(1, $return);
+        $this->assertDatabaseHas($this->table, $data);
+        $this->assertInstanceOf(LancamentoTcc::class, $fromRepository);
+        $this->assertEquals($data, $fromRepository->getOriginal());
+    }
+
+    public function testDelete()
+    {
+        $entry = factory(LancamentoTcc::class)->create();
+        $id = $entry->ltc_id;
+
+        $return = $this->repo->delete($id);
+
+        $this->assertEquals(1, $return);
+        $this->assertDatabaseMissing($this->table, $entry->toArray());
+    }
+
+    public function testLists()
+    {
+        $entries = factory(LancamentoTcc::class, 2)->create();
+
+        $model = new LancamentoTcc();
+        $expected = $model->pluck('ltc_titulo', 'ltc_id');
+        $fromRepository = $this->repo->lists('ltc_id', 'ltc_titulo');
+
+        $this->assertEquals($expected, $fromRepository);
+    }
+
+    public function testSearch()
+    {
+        $entries = factory(LancamentoTcc::class, 2)->create();
+
+        factory(LancamentoTcc::class)->create([
+            'ltc_titulo' => 'Tcc'
+        ]);
+
+        $searchResult = $this->repo->search(array(['ltc_titulo', '=', 'Tcc']));
+
+        $this->assertInstanceOf(TableCollection::class, $searchResult);
+        $this->assertEquals(1, $searchResult->count());
+    }
+
+    public function testSearchWithSelect()
+    {
+        factory(LancamentoTcc::class, 2)->create();
+
+        $entry = factory(LancamentoTcc::class)->create([
+            'ltc_titulo' => "Tcc"
+        ]);
+
+        $expected = [
+            'ltc_id' => $entry->ltc_id,
+            'ltc_titulo' => $entry->ltc_titulo
+        ];
+
+        $searchResult = $this->repo->search(array(['ltc_titulo', '=', "Tcc"]), ['ltc_id', 'ltc_titulo']);
+
+        $this->assertInstanceOf(TableCollection::class, $searchResult);
+        $this->assertEquals(1, $searchResult->count());
+        $this->assertEquals($expected, $searchResult->first()->toArray());
+    }
+
+    public function testAll()
+    {
+        // With empty database
+        $collection = $this->repo->all();
+
+        $this->assertEquals(0, $collection->count());
+
+        // Non-empty database
+        $created = factory(LancamentoTcc::class, 10)->create();
+        $collection = $this->repo->all();
+
+        $this->assertEquals($created->count(), $collection->count());
+    }
+
+    public function testCount()
+    {
+        $created = factory(LancamentoTcc::class, 10)->create();
+        $collection = $this->repo->all();
+
+        $this->assertEquals($created->count(), $this->repo->count());
+    }
+
+    public function testGetFillableModelFields()
+    {
+        $model = new LancamentoTcc();
+        $this->assertEquals($model->getFillable(), $this->repo->getFillableModelFields());
     }
 
     public function testPaginateWithoutParameters()
     {
-        factory(\Modulos\Academico\Models\LancamentoTcc::class, 2)->create();
+        factory(LancamentoTcc::class, 2)->create();
 
         $response = $this->repo->paginate();
 
         $this->assertInstanceOf(LengthAwarePaginator::class, $response);
-
         $this->assertGreaterThan(1, $response->total());
     }
 
     public function testPaginateWithSort()
     {
-        factory(\Modulos\Academico\Models\LancamentoTcc::class, 2)->create();
+        factory(LancamentoTcc::class, 2)->create();
 
         $sort = [
             'field' => 'ltc_id',
@@ -72,60 +172,33 @@ class LancamentoTccRepositoryTest extends TestCase
         $response = $this->repo->paginate($sort);
 
         $this->assertInstanceOf(LengthAwarePaginator::class, $response);
-
-        $this->assertGreaterThan(1, $response[0]->ltc_id);
+        $this->assertEquals(2, $response->first()->ltc_id);
     }
 
     public function testPaginateWithSearch()
     {
-        factory(\Modulos\Academico\Models\LancamentoTcc::class, 2)->create();
-
-        factory(\Modulos\Academico\Models\LancamentoTcc::class)->create([
-            'ltc_titulo' => 'Grafos Isomorfos',
+        factory(LancamentoTcc::class, 2)->create();
+        factory(LancamentoTcc::class)->create([
+            'ltc_titulo' => 'Tcc',
         ]);
 
         $search = [
             [
                 'field' => 'ltc_titulo',
-                'type' => 'like',
-                'term' => 'Grafos Isomorfos'
+                'type' => '=',
+                'term' => 'Tcc'
             ]
         ];
 
         $response = $this->repo->paginate(null, $search);
-
         $this->assertInstanceOf(LengthAwarePaginator::class, $response);
-
-        $this->assertCount(1, $response);
-    }
-
-    public function testPaginateWithSearchAndOrder()
-    {
-        factory(\Modulos\Academico\Models\LancamentoTcc::class, 2)->create();
-
-        $sort = [
-            'field' => 'ltc_id',
-            'sort' => 'desc'
-        ];
-
-        $search = [
-            [
-                'field' => 'ltc_id',
-                'type' => '>',
-                'term' => '1'
-            ]
-        ];
-
-        $response = $this->repo->paginate($sort, $search);
-
-        $this->assertInstanceOf(LengthAwarePaginator::class, $response);
-
         $this->assertGreaterThan(0, $response->total());
+        $this->assertEquals('Tcc', $response->first()->ltc_titulo);
     }
 
     public function testPaginateRequest()
     {
-        factory(\Modulos\Academico\Models\LancamentoTcc::class, 2)->create();
+        factory(LancamentoTcc::class, 2)->create();
 
         $requestParameters = [
             'page' => '1',
@@ -134,48 +207,8 @@ class LancamentoTccRepositoryTest extends TestCase
         ];
 
         $response = $this->repo->paginateRequest($requestParameters);
-
         $this->assertInstanceOf(LengthAwarePaginator::class, $response);
-
         $this->assertGreaterThan(0, $response->total());
-    }
-
-    public function testCreate()
-    {
-        $response = factory(\Modulos\Academico\Models\LancamentoTcc::class)->create();
-
-        $data = $response->toArray();
-
-        $this->assertInstanceOf(\Modulos\Academico\Models\LancamentoTcc::class, $response);
-
-        $this->assertArrayHasKey('ltc_id', $data);
-    }
-
-    public function testFind()
-    {
-        $data = factory(\Modulos\Academico\Models\LancamentoTcc::class)->create();
-
-        $data = $data->toArray();
-
-        // Retorna para date format americano antes de comparar com o banco
-        $data['ltc_data_apresentacao'] = Carbon::createFromFormat('d/m/Y', $data['ltc_data_apresentacao'])->toDateString();
-
-        $this->assertDatabaseHas('acd_lancamentos_tccs', $data);
-    }
-
-    public function testUpdate()
-    {
-        $data = factory(\Modulos\Academico\Models\LancamentoTcc::class)->create();
-
-        $updateArray = $data->toArray();
-        $updateArray['ltc_nome'] = 'abcde_edcba';
-
-        $lancamentotccId = $updateArray['ltc_id'];
-        unset($updateArray['ltc_id']);
-
-        $response = $this->repo->update($updateArray, $lancamentotccId, 'ltc_id');
-
-        $this->assertEquals(1, $response);
     }
 
     public function testUpdateReturn0()
@@ -261,17 +294,6 @@ class LancamentoTccRepositoryTest extends TestCase
         $response = $this->repo->findDisciplinaByTurma($turma->trm_id);
 
         $this->assertNotEmpty($response, '');
-    }
-
-
-    public function testDelete()
-    {
-        $data = factory(\Modulos\Academico\Models\LancamentoTcc::class)->create();
-        $lancamentotccId = $data->ltc_id;
-
-        $response = $this->repo->delete($lancamentotccId);
-
-        $this->assertEquals(1, $response);
     }
 
     public function tearDown()
