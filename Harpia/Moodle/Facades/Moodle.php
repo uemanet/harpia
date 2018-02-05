1b432779ef030f663ab2a64e84ff6cf0ae0cab15
Lucas S. Vieira <lucassouzavieiraengcomp@gmail.com>
2018-02-05 17:03:56 -0300

    Refatorado teste de MoodleServiceProvider

--- Harpia/Moodle/Facades/Moodle.php
+++ Harpia/Moodle/Facades/Moodle.php
@@ -4,6 +4,9 @@ namespace Harpia\Moodle\Facades;
 
 use Illuminate\Support\Facades\Facade;
 
+/**
+ * @codeCoverageIgnore
+ */
 class Moodle extends Facade
 {
     protected static function getFacadeAccessor()
