1dae0b2de01859a4e209f26ed96cdf2f832ee326
Pedro Fellipe <pedrofellipemelo123@gmail.com>
2018-04-06 15:34:35 -0300

    parâmetro extra adicionado em chamada de eventos ao disparar excessões nos listeners em que esse parâmetro é necessário

--- modulos/Academico/Listeners/DeleteOfertaDisciplinaListener.php
+++ modulos/Academico/Listeners/DeleteOfertaDisciplinaListener.php
@@ -63,7 +63,7 @@ class DeleteOfertaDisciplinaListener
                 throw $exception;
             }
 
-            event(new UpdateSincronizacaoEvent($event->getData(), 3, get_class($exception), $event->getAction()));
+            event(new UpdateSincronizacaoEvent($event->getData(), 3, get_class($exception), $event->getAction(), null, $event->getExtra()));
         } finally {
             return true;
         }
