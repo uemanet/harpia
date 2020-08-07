31d32bb1544d3188a9a0c7490b8a53e7be2a5535
DavidCarrilho <luan.david.oliver@gmail.com>
2020-08-07 15:52:51 -0300

    correção da inconsistência de dados em fontes pagadoras

--- modulos/RH/Http/Requests/FontePagadoraRequest.php
+++ modulos/RH/Http/Requests/FontePagadoraRequest.php
@@ -26,8 +26,20 @@ class FontePagadoraRequest extends BaseRequest
     public function rules()
     {
         $rules = [
-          'fpg_razao_social' => 'required|min:3|max:150',
-          'fpg_nome_fantasia' => 'required|min:3|max:150',
+            'fpg_razao_social' => 'required|min:3|max:150',
+            'fpg_nome_fantasia' => 'required|min:3|max:150',
+            'fpg_cnpj' => 'required|min:3|max:19',
+            'fpg_cep' => 'required|min:3|max:10',
+            'fpg_endereco' => 'required|min:3|max:150',
+            'fpg_bairro' => 'required|min:3|max:150',
+            'fpg_numero' => 'required|min:3|max:45',
+            'fpg_complemento' => 'required|min:3|max:150',
+            'fpg_cidade' => 'required|min:3|max:150',
+            'fpg_uf' => 'required|min:2|max:2',
+            'fpg_email' => 'required|min:3|max:150',
+            'fpg_telefone' => 'required|min:3|max:15',
+            'fpg_celular' => 'required|min:3|max:15',
+            'fpg_observacao' => 'required|min:3|max:150',
         ];
         return $rules;
     }
