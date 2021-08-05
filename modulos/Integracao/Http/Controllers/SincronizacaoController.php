90108a80a6998f211cf6721565bc1a0f7a9ea923
Pedro Fellipe Melo <pedrofellipemelo123@gmail.com>
2021-08-05 20:28:37 -0300

    feat: added verify false to postMapear

--- modulos/Integracao/Http/Controllers/SincronizacaoController.php
+++ modulos/Integracao/Http/Controllers/SincronizacaoController.php
@@ -177,7 +177,7 @@ class SincronizacaoController extends BaseController
             $url = $ambiente->amb_url . 'webservice/rest/server.php?wstoken=';
             $url .= $ambiente->integracao()->asr_token . '&wsfunction=local_integracao_map_user&moodlewsrestformat=json';
 
-            $client = new Client();
+            $client = new Client(['verify' => false ]);
 
             $response = $client->request('POST', $url, ['form_params' => $syncData['user']]);
 
