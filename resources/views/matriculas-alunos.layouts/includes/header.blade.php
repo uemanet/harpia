2402e0ed65de4695a6ccd271af9263e995de6fbe
Pedro Fellipe Melo <pedrofellipemelo123@gmail.com>
2022-01-24 23:43:56 -0300

    refactor: remove unused files

--- resources/views/matriculas-alunos.layouts/includes/header.blade.php
+++
@@ -1,7 +0,0 @@
-<header class="main-header">
-  <a href="{{url('/')}}" style="padding-top:2px" class="navbar-brand">
-    <img src="{{url('/')}}/img/logo.png" style="height:47px" />
-  </a>
-
-  @include('layouts.includes.header_rightmenu')
-</header>
--- resources/views/matriculas-alunos.layouts/includes/left.blade.php
+++
@@ -1,8 +0,0 @@
-<aside class="main-sidebar">
-   <!-- sidebar: style can be found in sidebar.less -->
-   <section class="sidebar">
-      <!-- sidebar menu: : style can be found in sidebar.less -->
-      {!!MasterMenu::render()!!}
-   </section>
-   <!-- /.sidebar -->
-</aside>
