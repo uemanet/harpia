f6388da70fed62274a4ba8432867c783c269f2ff
juniorcastr <76186762+juniorcastr@users.noreply.github.com>
2023-07-15 22:38:48 -0300

    Mostrar instituição na navbar (#124)

--- resources/views/layouts/includes/header_rightmenu.blade.php
+++ resources/views/layouts/includes/header_rightmenu.blade.php
@@ -7,12 +7,12 @@
             <li class="dropdown user user-menu">
                 <a href="#" class="dropdown-toggle" data-toggle="dropdown">
                     <img src="{{ route('seguranca.profile.profile-picture', \Illuminate\Support\Facades\Auth::user()->usr_profile_picture_id ?? 0) }}" class="user-image" alt="User Image">
-                    <span class="hidden-xs">{{ Auth::user()->pessoa->pes_nome }}</span>
+                    <span class="hidden-xs">{{ Auth::user()->pessoa->pes_nome }} @if(Auth::user()->pessoa->pes_itt_id) - {{ Auth::user()->pessoa->getInstituicaoSigla(Auth::user()->pessoa->pes_itt_id) }} @endif</span>
                 </a>
                 <ul class="dropdown-menu">
                     <li class="user-header">
                         <img src="{{ route('seguranca.profile.profile-picture', \Illuminate\Support\Facades\Auth::user()->usr_profile_picture_id ?? 0) }}" alt="User Image" class="img-circle">
-                        <p>{{ Auth::user()->pessoa->pes_nome }}</p>
+                        <p>{{ Auth::user()->pessoa->pes_nome }} @if(Auth::user()->pessoa->pes_itt_id) - {{ Auth::user()->pessoa->getInstituicaoSigla(Auth::user()->pessoa->pes_itt_id) }} @endif</p>
                     </li>
                     <li class="user-footer">
                         <div class="pull-left">
