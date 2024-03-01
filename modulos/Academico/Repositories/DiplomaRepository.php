a59de954268f766c7dfd24b60ac4d4193fbc24a1
Pedro Fellipe <pedrofellipemelo123@gmail.com>
2024-03-01 17:18:21 -0300

    feat: padroniza case sensitive em diplomas

--- modulos/Academico/Repositories/DiplomaRepository.php
+++ modulos/Academico/Repositories/DiplomaRepository.php
@@ -134,7 +134,7 @@ class DiplomaRepository extends BaseRepository
                 'ANO' => $diaatual->getAno(strtotime('today')),
             ];
 
-            $formata = str_replace('CURSO TÉCNICO EM ', '', $curso->crs_nome);
+            $formata = str_replace('CURSO TÉCNICO EM ', '', strtoupper($curso->crs_nome));
             $formata = str_replace('CURSO TÉCNICO ', '', $formata);
             $cursonome = $this->ucwords_improved(mb_strtolower($formata, "UTF-8"), array('e', 'em', 'da', 'das', 'do', 'de'));
 
