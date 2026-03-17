ce0ba0cfd33f9d0876ccc7cc6a95aa773b8eeb0d
juniorcastr <juniorcastrz@gmail.com>
2026-03-17 09:09:43 -0300

    Fix: $routeMiddleware no Kernel.php que foi necessário numa versão passada.

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -46,7 +46,7 @@ class Kernel extends HttpKernel
      *
      * @var array
      */
-    protected $middlewareAliases = [
+    protected $routeMiddleware = [
         'auth' => \App\Http\Middleware\Authenticate::class,
         'auth.basic' => \Illuminate\Auth\Middleware\AuthenticateWithBasicAuth::class,
         'bindings' => \Illuminate\Routing\Middleware\SubstituteBindings::class,
