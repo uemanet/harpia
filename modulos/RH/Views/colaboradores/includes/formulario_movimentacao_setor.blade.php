34a50636fc4fdc3e95aaa3fb4041dcf3976b6e04
juniorcastr <juniorcastrz@gmail.com>
2026-03-17 18:01:31 -0300

    fix: corrige parâmetros ausentes em rotas e ajusta bindings em formulários

--- modulos/RH/Views/colaboradores/includes/formulario_movimentacao_setor.blade.php
+++ modulos/RH/Views/colaboradores/includes/formulario_movimentacao_setor.blade.php
@@ -5,7 +5,7 @@
             <select name="col_set_id" class="form-control">
     <option value="">Selecione o setor</option>
     @foreach($setores as $key => $value)
-        <option value="{{ $key }}" {{ isset($colaborador->col_set_id) ? $colaborador->col_set_id : old('col_set_id') == $key ? 'selected' : '' }}>{{ $value }}</option>
+        <option value="{{ $key }}" {{ old('col_set_id', isset($colaborador->col_set_id) ? $colaborador->col_set_id : null) == $key ? 'selected' : '' }}>{{ $value }}</option>
     @endforeach
 </select>
             @if ($errors->has('col_set_id')) <p class="help-block">{{ $errors->first('col_set_id') }}</p> @endif
