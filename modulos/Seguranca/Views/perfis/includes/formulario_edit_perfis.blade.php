de9968c0267761157d828d57a1e3434788591f24
Thomaz Machado <thomaz.cto@xferatech.com>
2026-03-18 16:19:10 -0300

    fix module seguranca views

--- modulos/Seguranca/Views/perfis/includes/formulario_edit_perfis.blade.php
+++ modulos/Seguranca/Views/perfis/includes/formulario_edit_perfis.blade.php
@@ -5,7 +5,7 @@
             <select name="prf_mod_id" class="form-control" id="mod_id">
                 <option value="">Selecione um módulo</option>
                 @foreach($modulos as $key => $value)
-                    <option value="{{ $key }}" {{ old('prf_mod_id', $perfil->prf_mod_id ?? '') == $key ? 'selected' : '' }}>{{ $value }}</option>
+                    <option value="{{ $key }}" {{ old('prf_mod_id') == $key ? 'selected' : '' }}>{{ $value }}</option>
                 @endforeach
             </select>
             @if ($errors->has('prf_mod_id')) <p class="help-block">{{ $errors->first('prf_mod_id') }}</p> @endif
@@ -14,7 +14,7 @@
     <div class="form-group col-md-9 @if ($errors->has('prf_nome')) has-error @endif">
         <label for="prf_nome" class="control-label">Nome do perfil*</label>
         <div class="controls">
-            <input type="text" name="prf_nome" value="{{ old('prf_nome', $perfil->prf_nome ?? '') }}" class="form-control" >
+            <input type="text" name="prf_nome" value="{{ old('prf_nome') }}" class="form-control" >
             @if ($errors->has('prf_nome')) <p class="help-block">{{ $errors->first('prf_nome') }}</p> @endif
         </div>
     </div>
@@ -22,7 +22,7 @@
 <div class="form-group @if ($errors->has('prf_descricao')) has-error @endif">
     <label for="prf_descricao" class="control-label">Descrição do perfil</label>
     <div class="controls">
-        <input type="text" name="prf_descricao" value="{{ old('prf_descricao', $perfil->prf_descricao ?? '') }}" class="form-control" >
+        <input type="text" name="prf_descricao" value="{{ old('prf_descricao') }}" class="form-control" >
         @if ($errors->has('prf_descricao')) <p class="help-block">{{ $errors->first('prf_descricao') }}</p> @endif
     </div>
 </div>
--- modulos/Seguranca/Views/usuarios/includes/formulario_user.blade.php
+++ modulos/Seguranca/Views/usuarios/includes/formulario_user.blade.php
@@ -2,15 +2,14 @@
     <div class="form-group col-md-4 @if ($errors->has('usr_usuario')) has-error @endif">
         <label for="usr_usuario" class="form-label">Usuário de acesso*</label>
         <div class="controls">
-            <input type="text" name="usr_usuario" value="{{ old('usr_usuario', $usuario->usr_usuario ?? '') }}" class="form-control" >
+            <input type="text" name="usr_usuario" value="{{ old('usr_usuario') }}" class="form-control" >
             @if ($errors->has('usr_usuario')) <p class="help-block">{{ $errors->first('usr_usuario') }}</p> @endif
         </div>
     </div>
     <div class="form-group col-md-4 @if ($errors->has('usr_senha')) has-error @endif">
         <label for="usr_senha" class="form-label">Senha*</label>
         <div class="controls">
-            {{-- Senha geralmente não se retorna no old() por segurança --}}
-            <input type="password" name="usr_senha" class="form-control" >
+            <input type="password" name="usr_senha" >
             @if ($errors->has('usr_senha')) <p class="help-block">{{ $errors->first('usr_senha') }}</p> @endif
         </div>
     </div>
@@ -18,8 +17,8 @@
         <label for="usr_ativo" class="form-label">Ativo*</label>
         <div class="controls">
             <select name="usr_ativo" class="form-control">
-                <option value="1" {{ old('usr_ativo', $usuario->usr_ativo ?? 1) == 1 ? 'selected' : '' }}>Sim</option>
-                <option value="0" {{ old('usr_ativo', $usuario->usr_ativo ?? 1) == 0 ? 'selected' : '' }}>Não</option>
+                <option value="1" {{ old('usr_ativo', 1) == 1 ? 'selected' : '' }}>Sim</option>
+                <option value="0" {{ old('usr_ativo', 1) == 0 ? 'selected' : '' }}>Não</option>
             </select>
             @if ($errors->has('usr_ativo')) <p class="help-block">{{ $errors->first('usr_ativo') }}</p> @endif
         </div>
